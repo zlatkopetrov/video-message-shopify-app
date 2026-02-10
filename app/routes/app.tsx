f3bb5f81a83ea9b47ef4fd5d4cc32ea7b5443661
Zack Petrov <zlate55@gmail.com>
2026-02-10 18:17:58 +1000

    removed aditional page

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,9 +1,10 @@
 import type { HeadersFunction, LoaderFunctionArgs } from "react-router";
-import { Outlet, useLoaderData, useRouteError } from "react-router";
-import { boundary } from "@shopify/shopify-app-react-router/server";
-import { AppProvider } from "@shopify/shopify-app-react-router/react";
+import { Outlet, useLoaderData, useRouteError } from 'react-router';
 
-import { authenticate } from "../shopify.server";
+import { AppProvider } from '@shopify/shopify-app-react-router/react';
+import { boundary } from '@shopify/shopify-app-react-router/server';
+
+import { authenticate } from '../shopify.server';
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   await authenticate.admin(request);
@@ -19,7 +20,6 @@ export default function App() {
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
         <s-link href="/app">Home</s-link>
-        <s-link href="/app/additional">Additional page</s-link>
       </s-app-nav>
       <Outlet />
     </AppProvider>
